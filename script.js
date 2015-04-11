047e75c372e2338396acb186522f76a21f8777c6
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-04-11 13:34:24 -0500

    add cat image 4,5 and click list shows correct img

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var elem = document.getElementById('catImage');
+var image = document.getElementById('catImage');
 var name = document.getElementById('catName');
 var clicks = document.getElementById('numClicks');
 
@@ -6,7 +6,7 @@ var namesArray = ['Cat 1','Cat 2', 'Cat 3', 'Cat 4', 'Cat 5']
 var clicksArray = [0,0,0,0,0]
 
 
-elem.addEventListener('click', function(){
+image.addEventListener('click', function(){
 	var clicksCount = document.getElementById('numClicks').innerHTML;
 	document.getElementById('numClicks').innerHTML = parseInt(clicksCount) + 1
 	name.innerHTML = "Cat 2"
@@ -15,24 +15,18 @@ elem.addEventListener('click', function(){
 
 var nums = [1,2,3,4,5];
 
-// Let's loop over the numbers in our array
 for (var i = 0; i < nums.length; i++) {
 
     // This is the number we're on...
     var num = nums[i];
-    // console.log('catLink' + nums[i]);
-    // We're creating a DOM element for the number
     var elem = document.getElementById('catLink' + nums[i]);
-    // elem.textContent = num;
-
     // ... and when we click, alert the value of `num`
+    // IFFE
     elem.addEventListener('click', (function(numCopy) {
         return function() {
-            alert(numCopy);
+            image.src = "cat" + numCopy + ".jpg"
         };
     })(num));
-
-    // document.body.appendChild(elem);
 };
 
 
